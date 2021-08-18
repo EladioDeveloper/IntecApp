6cddea9b50041f9af8ceecfb59f87a9288d8ced3
Eladio Junior Rodriguez Rodriguez <eladiojuniorrodriguez@gmail.com>
2021-08-18 01:11:21 -0400

    initial Commit

---
+++ LoginApp/LoginApp/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LoginApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void LoginButton_Clicked(object sender, EventArgs e)
+        {
+            if (ID.Text == "" || Password.Text == "")
+                await DisplayAlert("Error", "Campos Vacios", "OK");
+            else
+                await DisplayAlert("Bienvenido", $"Hola {ID.Text}", "OK");
+        }
+    }
+}
